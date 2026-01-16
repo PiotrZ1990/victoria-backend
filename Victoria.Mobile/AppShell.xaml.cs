cb2802830237e5f862f043b760618d45d66c9406
Piotr <piotr@student.local>
2026-01-16 17:21:45 +0100

    (mobile): main page scaffold

--- Victoria.Mobile/AppShell.xaml.cs
+++ Victoria.Mobile/AppShell.xaml.cs
@@ -1,10 +1,9 @@
-﻿namespace Victoria.Mobile
+﻿namespace Victoria.Mobile;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
